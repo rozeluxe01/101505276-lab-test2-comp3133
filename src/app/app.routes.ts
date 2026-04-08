b6827008062b5b4f1af6fa99bb134bc5d1715853
Kevin George Buhain <keving.buhain@gmail.com>
2026-04-08 11:51:33 -0400

    removed unused components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import { Routes } from '@angular/router';
 import { CharacterlistComponent } from './components/characterlist/characterlist';
 import { CharacterdetailsComponent } from './components/characterdetails/characterdetails';
-import { CharacterfilterComponent } from './components/characterfilter/characterfilter';
-
 
 export const routes: Routes = [
   { path: '', component: CharacterlistComponent },
-  { path: 'filter', component: CharacterfilterComponent },
   { path: 'details/:id', component: CharacterdetailsComponent }
 ];
